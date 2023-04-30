c78ecb4b61eaae42b32da631e207b10a3ceb2d54
nadeem22 <nadeem2107@gmail.com>
2023-04-30 11:40:28 +0530

    updated

--- dropown.java
+++ dropown.java
@@ -11,12 +11,34 @@
         });
     }
 	
-public List<Team> getTeamDropdown(String city, UserForm userLogin) {
-        List<Object> params = new ArrayList<>();
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TeamDropdown {
+    private JdbcTemplate jdbcTemplate;
+
+    public TeamDropdown() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/mydb");
+        dataSource.setUsername("username");
+        dataSource.setPassword("password");
+
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public ArrayList<Team> getTeamDropdown(String city, UserForm userLogin) {
         String sql = "";
+        List<Object> params = new ArrayList<>();
 
         if (city != null && !city.equals("-1")) {
-            sql = "SELECT team_name, team_id FROM h2h_teams where team_id not in (select op_team_id from op_team) and city_id=? ";
+            sql = "SELECT team_name, team_id FROM h2h_teams WHERE team_id NOT IN (SELECT op_team_id FROM op_team) AND city_id=?";
             params.add(city);
 
             if (!userLogin.getUserType().equals("1") && !userLogin.getUserType().equals("17")) {
@@ -24,21 +46,29 @@ public List<Team> getTeamDropdown(String city, UserForm userLogin) {
                 params.add(userLogin.getUserId());
             }
         } else {
-            sql = "SELECT team_name, team_id FROM h2h_teams ";
+            sql = "SELECT team_name, team_id FROM h2h_teams";
 
             if (userLogin.getUserType() != null && !userLogin.getUserType().equals("1") && !userLogin.getUserType().equals("17")) {
-                sql += " where agencyId=? and team_id not in (select op_team_id from op_team)";
+                sql += " WHERE agencyId=? AND team_id NOT IN (SELECT op_team_id FROM op_team)";
                 params.add(userLogin.getUserId());
             }
         }
+        sql += " ORDER BY team_name ASC";
 
-        sql += " order by team_name asc";
-        System.out.println(sql);
-
-        return jdbcTemplate.query(sql, params.toArray(), (rs, rowNum) -> {
+        ArrayList<Team> teamList = (ArrayList<Team>) jdbcTemplate.query(sql, new PreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement preparedStatement) throws SQLException {
+                for (int i = 0; i < params.size(); i++) {
+                    preparedStatement.setObject(i + 1, params.get(i));
+                }
+            }
+        }, (rs, rowNum) -> {
             Team team = new Team();
             team.setTeamName(rs.getString("team_name"));
             team.setTeamId(rs.getString("team_id"));
             return team;
         });
+
+        return teamList;
     }
+}
