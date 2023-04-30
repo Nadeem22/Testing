9f715f9e51e286f79930817f7bed64db7f9b9462
Mohammad Nadeem <nadeem2107@gmail.com>
2023-04-30 17:49:25 +0530

    Update adduserdao.java

--- adduserdao.java
+++ adduserdao.java
@@ -1,15 +1,15 @@
 public HashMap<String, Object> getUserImages(String userId) {
-        HashMap<String, Object> result = new HashMap<>();
         String query = "SELECT rejected_imgpath, remark as remark FROM profile_image_status WHERE user_id=?";
 
-        jdbcTemplate.query(query, new PreparedStatementSetter() {
+        return jdbcTemplate.query(query, new PreparedStatementSetter() {
             @Override
             public void setValues(PreparedStatement preparedStatement) throws SQLException {
                 preparedStatement.setString(1, userId);
             }
-        }, new ResultSetExtractor<Void>() {
+        }, new ResultSetExtractor<HashMap<String, Object>>() {
             @Override
-            public Void extractData(ResultSet rs) throws SQLException {
+            public HashMap<String, Object> extractData(ResultSet rs) throws SQLException {
+                HashMap<String, Object> result = new HashMap<>();
                 while (rs.next()) {
                     String userImage = rs.getString("rejected_imgpath");
                     result.put("userImage", ConstantKeys.S3_PATH + userImage);
@@ -17,9 +17,7 @@ public HashMap<String, Object> getUserImages(String userId) {
                     String remark = rs.getString("remark");
                     result.put("remark", remark);
                 }
-                return null;
+                return result;
             }
         });
-
-        return result;
     }
