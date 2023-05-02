1fa9b69ec02a3c53ca2136400ab08553a8d0c9e9
Mohammad Nadeem <nadeem2107@gmail.com>
2023-05-02 16:19:31 +0530

    Update adduserdao.java

--- adduserdao.java
+++ adduserdao.java
@@ -21,3 +21,59 @@ public HashMap<String, Object> getUserImages(String userId) {
             }
         });
     }
+@Autowired
+private JdbcTemplate jdbcTemplate;
+
+public List<User> getUser(String userName, String userType, String city, String team, String page,
+        UserForm loginUser, String value, String param) {
+    System.out.println("page " + page);
+    List<User> userList = new ArrayList<>();
+
+    // ... (the same query building logic as in the original method) ...
+
+    StringBuilder queryBuilder = new StringBuilder(query);
+
+    int start = ContextUtil.getReportProperties("report_start");
+    int limitValue = ContextUtil.getReportProperties(param);
+    if (!value.equals("0")) {
+        int values = Integer.parseInt(value);
+        queryBuilder.append("  limit ").append(values * limitValue).append(",").append(limitValue + 1);
+    } else {
+        queryBuilder.append(" limit ").append(start).append(",").append(limitValue + 1);
+    }
+
+    String finalQuery = queryBuilder.toString();
+
+    userList = jdbcTemplate.query(finalQuery, preparedStatementSetter(userName, userType, city, team, page), (ResultSetExtractor<List<User>>) rs -> {
+        List<User> users = new ArrayList<>();
+
+        while (rs.next()) {
+            User user = new User();
+            // ... (the same user object population logic as in the original method) ...
+            users.add(user);
+        }
+        return users;
+    });
+
+    return userList;
+}
+
+private PreparedStatementSetter preparedStatementSetter(String userName, String userType, String city, String team, String page) {
+    return preparedStatement -> {
+        if (page.equals("searchPage")) {
+            int index = 0;
+            if (!(userName == null || userName.equals("-1"))) {
+                preparedStatement.setString(++index, "%" + userName + "%");
+            }
+            if (!(userType == null || userType.equals("-1"))) {
+                preparedStatement.setString(++index, userType);
+            }
+            if (!(city == null || city.equals("-1"))) {
+                preparedStatement.setString(++index, city);
+            }
+            if (!(team == null || team.equals("-1"))) {
+                preparedStatement.setString(++index, team);
+            }
+        }
+    };
+}
