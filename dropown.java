ae52238d59b0893bcf5e42711024463ca8091296
Mohammad Nadeem <nadeem2107@gmail.com>
2023-05-19 11:09:08 +0530

    Update dropown.java

--- dropown.java
+++ dropown.java
@@ -72,3 +72,72 @@ public class TeamDropdown {
         return teamList;
     }
 }
+
+
+
+public String generateDropdowns(HttpSession session, String agencyId) {
+    try {
+        String userType = (String) session.getAttribute("UserType");
+
+        String sql = "SELECT CITY_NAME, CITY_ID FROM CITY_MASTER WHERE 1=1 ";
+        if(userType.equals("25")){
+            sql += " AND ZONE = 3 ";
+        }
+        if(userType.equals("26")){
+            sql += " AND ZONE IN (0,1,2) ";
+        }
+        sql += "ORDER BY CITY_NAME ASC";
+        List<City> cityList = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(City.class));
+        session.setAttribute("cityList", cityList);
+
+        sql = "SELECT st.STATE_NAME, st.STATE_ID FROM STATE_MASTER st ";
+        if(userType.equals("25")){
+            sql += " inner join city_master ct on ct.state_id = st.state_id where zone = 3";
+        }
+        if(userType.equals("26")){
+            sql += " inner join city_master ct on ct.state_id = st.state_id where zone in (0,1,2)";
+        }
+        List<State> stateList = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(State.class));
+        session.setAttribute("stateList", stateList);
+
+        sql = "SELECT USER_NAME FROM USER_MASTER";
+        if (!userType.equals("1")) {
+            sql += " Where agencyId=" + agencyId;
+        }
+        List<String> userList = jdbcTemplate.queryForList(sql, String.class);
+        session.setAttribute("userList", new Gson().toJson(userList));
+
+        if (userType.equals("10")) {
+            String city = (String) session.getAttribute("usercity");
+            int zone = DropDownListDao.getZoneByCity(city);
+            sql = "SELECT TEAM_ID, TEAM_NAME FROM h2h_TEAMS where city_id in (select city_id from city_master where zone=" + zone + ") AND agencyId=" + agencyId + " Order by TEAM_NAME asc ";
+        } else if(userType.equals("25")){
+            sql = "SELECT TEAM_ID, TEAM_NAME FROM h2h_TEAMS where city_id in (select city_id from city_master where zone= 3) AND agencyId=" + agencyId + " Order by TEAM_NAME asc ";
+        } else if(userType.equals("26")){
+            sql = "SELECT TEAM_ID, TEAM_NAME FROM h2h_TEAMS where city_id in (select city_id from city_master where zone in (0,1,2)) AND agencyId=" + agencyId + " Order by TEAM_NAME asc ";
+        } else {
+            if(userType.equals("1")){
+                sql = "SELECT TEAM_ID, TEAM_NAME FROM h2h_TEAMS where 1=1"; 
+            }else{
+                sql = "SELECT TEAM_ID, TEAM_NAME FROM h2h_TEAMS where agencyId not in (select user_id from dummy_agencies)";
+            }
+            if (!userType.equals("1") && !userType.equals("17")&& !userType.equals("15") && agencyId != null) {
+                sql += "and agencyId=" + agencyId;
+            }
+        }
+        sql += "  Order by TEAM_NAME asc ";
+        List<Team> teamNameList = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Team.class));
+        session.setAttribute("teamNameList", teamNameList);
+
+        if(!userType.equals(18)){
+            sql = "SELECT Field_Data_ID,Field_Data_Name,Field_Data_Details FROM BRANDS Where ParentId is null and disabled='N' order by Field_Data_Name";
+            List<Brands> brandList = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Brands.class));
+            session.setAttribute("brandList", brandList);
+        }
+
+        return "success";
+    } catch (DataAccessException e) {
+        e.printStackTrace();
+        return null;
+    }
+}
